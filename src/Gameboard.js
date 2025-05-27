8ee20f32a819482baceb7dfd3dd56a8a85cf7302
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-27 12:58:29 -0400

    feat(gameboard class): adds Gameboard class and constructor

---
+++ src/Gameboard.js
@@ -0,0 +1,15 @@
+
+export default class Gameboard{
+
+    constructor({size = 1}){
+        this.size = size;
+        this.ships = [];
+        this.missedAttacks = [];
+        this.isAllSunk = false;
+    }
+
+
+    
+
+
+}
