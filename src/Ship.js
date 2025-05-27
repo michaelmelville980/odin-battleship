6bf6228dd8e59d7b58a3dd721657d29cb74f3501
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-27 10:35:29 -0400

    feat(class): adds Ship class with constructor and empty method skeletons

---
+++ src/Ship.js
@@ -0,0 +1,18 @@
+
+export default class Ship{
+
+    constructor({length = 0, timesHit = 0, sunk = false}){
+        this.length = length;
+        this.timesHit = timesHit;
+        this.sunk = sunk;
+    }
+
+    hit(){
+
+    }
+
+    isSunk(){
+
+    }
+
+}
