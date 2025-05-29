ad2e8bbaba195187ef8c3b54295d7ec14beedcdf
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-29 16:27:02 -0400

    fix(view): fixes coordinate system so (0,0) is bottom right of rendered board

--- src/view.js
+++ src/view.js
@@ -1,34 +1,33 @@
-
 import Gameboard from './Gameboard.js';
 import Player from './Player.js';
 
-const boards = document.querySelector("#boards");
+export default class view {
+    
+    static renderBoard(player = new Player(), parentDiv) {
+        parentDiv.innerHTML = '';
 
-export default class view{
+        for (let y = 0; y < player.gameboard.size; y++) {
+            for (let x = 0; x < player.gameboard.size; x++) {
 
-    renderBoard(player = new Player(), parentDiv){
+            /* Creating cell and adding shared, row num, and column num classes */
+            let cell = document.createElement('div');
+            cell.classList.add('cell', 'x:' + x, 'y:' + y);
 
-        for (let x = 0; x < player.gameboard.size; x++){
-            for (let y = 0; y < player.gameboard.size; y++){
+            /* Manually positioning cell */
+            cell.style.gridRow = 10 - y;
+            cell.style.gridColumn = x + 1;
 
-                /* Creating cell and adding shared, row num, and column num classes */
-                let cell = document.createElement("div");
-                cell.classList.add("cell", "row" + x, "column" + y);
+            /* Checking for missed/successful attacks*/
+            if (player.gameboard.missedAttacks.has(`${x},${y}`)) {
+                cell.classList.add('missedAttack');
+            } else if (player.gameboard.successfulAttacks.has(`${x},${y}`)) {
+                cell.classList.add('successfulAttack');
+            }
 
-                 /* Checking for missed/successful attacks*/
-                if (player.gameboard.missedAttacks.includes([x, y])){
-                    cell.classList.add("missedAttack");
-                }else if (player.gameboard.successfulAttacks.includes([x, y])){
-                    cell.classList.add("successfulAttack");
-                }
-
-                /* Adding cell to div */
-                parentDiv.appendChild(cell);
-
+            /* Adding cell to div */
+            parentDiv.appendChild(cell);
             }
         }
-
     }
-
 
 }
